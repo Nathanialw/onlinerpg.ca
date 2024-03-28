747a951dc5e5bbe4bbf9eb85595295d769972c8a
nathanial <nathanial28@gmail.com>
2024-03-28 13:29:27 -0600

    retest

--- src/keyboard_input.js
+++ src/keyboard_input.js
@@ -11,47 +11,49 @@ var sound = new Howl ({
 document.addEventListener("keydown", (event) => {
     const keyName = event.key;
     
-    if (keyName === "space") {
-        sound.play();                
-        // do not alert when only Control key is pressed.
+    if (keyName === "w") {
+        sprite.y -= 5;
+        return;
+    }
+    
+    if (keyName === "a") {
+        sprite.x -= 5;
+        return;
+    }
+    
+    if (keyName === "s") {
+        sprite.y += 5;
+        return;
+    }
+    
+    if (keyName === "d") {
+        sprite.x += 5;
+        return;
+    }
+    
+    
+    if (keyName === "Enter") {
+        sound.play();
+        socket.send("The Spacebar has been pressed");            
         return;
     }
 
-        if (keyName === "w") {
-            sprite.y -= 5;
-            return;
-        }
 
-        if (keyName === "a") {
-            sprite.x -= 5;
-            return;
-        }
+    if (event.shiftKey) {
+        sound.play();
+        socket.send("The Spacebar has been pressed");            
+        return;
+    }
 
-        if (keyName === "s") {
-            sprite.y += 5;
-            return;
-        }
-
-        if (keyName === "d") {
-            sprite.x += 5;
-            return;
-        }
-
-        if (keyName === "space") {
-            sound.play();
-            socket.send("The Spacebar has been pressed");            
-            return;
-        }
-
-        if (event.ctrlKey) {
-        // Even though event.key is not 'Control' (e.g., 'a' is pressed),
-        // event.ctrlKey may be true if Ctrl key is pressed at the same time.
-            sound.play();                
-        } 
-        // W, A, S, D || UP, DOWN, LEFT, RIGHT
-        // 1, 2, 4, 5, 6, etc... TO CAST SPELLS
-        // B, C, P, I, etc... UI BUTTONS
-
+    if (event.ctrlKey) {
+    // Even though event.key is not 'Control' (e.g., 'a' is pressed),
+    // event.ctrlKey may be true if Ctrl key is pressed at the same time.
+        sound.play();    
+        return;            
+    } 
+    // W, A, S, D || UP, DOWN, LEFT, RIGHT
+    // 1, 2, 4, 5, 6, etc... TO CAST SPELLS
+    // B, C, P, I, etc... UI BUTTONS
     }, 
     false,
 );
