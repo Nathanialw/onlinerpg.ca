5f300165c929663adedf23b2bd4f7bdae77de305
nathanial <nathanial28@gmail.com>
2024-03-30 06:53:38 -0600

    added random units from server

---
+++ src/input/mouse.js
@@ -0,0 +1,10 @@
+
+
+function Interrogate_Cell() {
+
+    // it should return the uID of the unit in the cell
+    let cell = Get_Cell_At_Mouse_Position();
+    if (cell) {
+        console.log(cell);
+    }
+}
