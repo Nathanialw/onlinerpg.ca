2a234e71f6fa67f1a7a802fcd2b6a96fbebaf595
nathanial <nathanial28@gmail.com>
2024-03-30 17:35:31 -0600

    refactoring the UI php files

--- public/signup.php
+++ public/signup.php
@@ -1,15 +1,7 @@
 <?php
     include 'header.php'
 
-    if (isset($_POST["submit"])) {
-        $_POST = array_map('trim', $_POST);
-        echo $_POST["name"];
-        echo $_POST["email"];
-        echo $_POST["uid"];
-        echo $_POST["pwd"];
-        echo $_POST["pwdrepeat"];
-        // include '/var/www/server/privatefolder/scripts/includes/signup.h.php';
-    }
+
 ?>
 
     <section class="signup-form">
@@ -28,7 +20,15 @@
 
 <?php
 
-
+    if (isset($_POST["submit"])) {
+        $_POST = array_map('trim', $_POST);
+        echo $_POST["name"];
+        echo $_POST["email"];
+        echo $_POST["uid"];
+        echo $_POST["pwd"];
+        echo $_POST["pwdrepeat"];
+        // include '/var/www/server/privatefolder/scripts/includes/signup.h.php';
+    }
 
     if (isset($_GET["error"])) {
         if ($_GET["error"] == "emptyinput") {
