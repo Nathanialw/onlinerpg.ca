0a2cb5aacf8ee66d1f407907e0326fddef724c88
nathanial <nathanial28@gmail.com>
2024-03-30 18:37:45 -0600

    refactoring the UI php files

--- private/include/signup.h.php
+++ private/include/signup.h.php
@@ -1,13 +1,8 @@
 <?php
 
 
-function CheckSubmit($_POST) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $username = $_POST['uid'];
-    $pwd = $_POST['pwd'];
-    $pwdRepeat = $_POST['pwdrepeat'];
-    
+function CheckSubmit($name, $email, $username, $pwd, $pwdRepeat) {
+
     require_once 'dbh.h.php';
     require_once 'functions.h.php';
     
