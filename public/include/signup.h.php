35d165e8043a349f03916193739bb10f939181f8
nathanial <nathanial28@gmail.com>
2024-03-30 18:41:15 -0600

    refactoring the UI php files

--- public/include/signup.h.php
+++ public/include/signup.h.php
@@ -13,33 +13,31 @@ if (isset($_POST["submit"])) {
     $pwd = $_POST['pwd'];
     $pwdRepeat = $_POST['pwdrepeat'];
     
-
-    if ($check = CheckSubmit($name, $email, $username, $pwd, $pwdRepeat)) {
+    $check = CheckSubmit($name, $email, $username, $pwd, $pwdRepeat);
         
-        if (check == 1 ) {
-            header("Location: ../signup.php?error=emptyinput");
-            exit();   
-        }
-        else if (check == 2) {
-            header("Location: ../signup.php?error=invaliduid");
-            exit();
-        }
-        else if (check == 3) {
-            header("Location: ../signup.php?error=invalidemail");
-            exit();
-        }
-        else if (check == 4) {
-            header("Location: ../signup.php?error=passwordsdontmatch");
-            exit();
-        }
-        // else if (check == 5) {
-        //     header("Location: ../signup.php?error=usernametaken");
-        //     exit();
-        // }        
-    }
-
-    else {
+    if ($check == 0) {
         header("Location: ../signup.php");
         exit();
     }
+    else if ($check == 1 ) {
+        header("Location: ../signup.php?error=emptyinput");
+        exit();   
+    }
+    else if ($check == 2) {
+        header("Location: ../signup.php?error=invaliduid");
+        exit();
+    }
+    else if ($check == 3) {
+        header("Location: ../signup.php?error=invalidemail");
+        exit();
+    }
+    else if ($check == 4) {
+        header("Location: ../signup.php?error=passwordsdontmatch");
+        exit();
+
+    else if ($check == 5) {
+        header("Location: ../signup.php?error=usernametaken");
+        exit();
+    }        
+
 }
