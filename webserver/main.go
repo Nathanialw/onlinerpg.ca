cb67af23ade3806dab0bdd1e41ed98046025633a
nathanial <nathanial28@gmail.com>
2024-04-04 23:38:11 -0600

    added webserver

---
+++ webserver/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type PageData struct {
+	Title string
+	Body  string
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData{
+			Title: "My Page Title",
+			Body:  "Welcome to my website!",
+		}
+
+		tmpl, err := template.ParseFiles("template.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
