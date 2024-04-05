1197931de1ed08b53e9e5b03e82a986aa54a4296
nathanial <nathanial28@gmail.com>
2024-04-05 00:34:19 -0600

    added web server

--- webserver/main.go
+++ webserver/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"html/template"
 	"net/http"
 )
@@ -11,23 +13,42 @@ type PageData struct {
 }
 
 func main() {
-	http.HandleFunc("/goapp", func(w http.ResponseWriter, r *http.Request) {
-		data := PageData{
-			Title: "My Page Title",
-			Body:  "Welcome to my website!",
-		}
+	r := httprouter.New()
+	r.GET("/goapp", homeHandler)
+	server := http.Server{
+		Addr:    "localhost:9001",
+		Handler: r,
+	}
 
-		tmpl, err := template.ParseFiles("template.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
-		err = tmpl.Execute(w, data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+func homeHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Printf("message received from %s\n"+p.ByName("name"), r.RemoteAddr)
 
-	http.ListenAndServe(":9001", nil)
+	data := PageData{
+		Title: "My Page Title",
+		Body:  "Welcome to my dwebsite!",
+	}
+	tmpl, err := template.ParseFiles("template.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle requests for individual products here
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle requests for the user API here
 }
