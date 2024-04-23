78df21ed554bb36728c541c59011b6736b77bf98
nathanial <nathanial28@gmail.com>
2024-04-23 09:42:25 -0600

    map is now static on server startup

--- public/js/movement/movement.js
+++ public/js/movement/movement.js
@@ -10,28 +10,28 @@ const cell_size = 24;
 function Move(keyName) {
     if (keyName === "w") {
         Move_Player(0, -cell_size);
-        socket.send("1w");       
+        socket().send("1w");       
         //send to server
         return true;
     }
     
     else if (keyName === "a") {
         Move_Player(-cell_size, 0);
-        socket.send("1a");       
+        socket().send("1a");       
         //send to server
         return true;
     }
     
     else if (keyName === "s") {
         Move_Player(0, cell_size)
-        socket.send("1s");       
+        socket().send("1s");       
         //send to server
         return true;
     }
     
     else if (keyName === "d") {
         Move_Player(cell_size, 0);
-        socket.send("1d");       
+        socket().send("1d");       
         //send to server
         return true;
     }
