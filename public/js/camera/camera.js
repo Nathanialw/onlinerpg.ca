48930f039f00cc8ea42a1868e92957666d2a57d6
nathanial <nathanial28@gmail.com>
2024-09-11 10:14:17 -0600

    added start button

--- public/js/camera/camera.js
+++ public/js/camera/camera.js
@@ -1,2 +1,11 @@
 import app from '../frontend/game.js';
 
+///every time it moves
+    //send message to server
+    //recieve message from server
+
+///every time it moves 
+    //send movement to server
+    //recieve new map section from server
+    //update the cells from the map section array
+    //redraw the canvas
