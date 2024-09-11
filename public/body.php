8afa12117ff461a4bc8ebedfd32182fca263bfe6
nathanial <nathanial28@gmail.com>
2024-09-10 19:43:53 -0600

    fixed input

--- public/body.php
+++ public/body.php
@@ -3,10 +3,13 @@
 
 
         <!-- main content -->
-        <div id="gameCanvas">
+        <div id="wrapper">
             <br>
             <br>
         </div>
+        
+        <div id="gameCanvas"></div>
+        
         <script src="js/input/keyboard.js" type="module"></script>
         <script src="js/libs/howler.core.js"></script>
         <script src="js/libs/pixi.js"></script>
