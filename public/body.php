b9ecb8fc1661c3406bc9c7e261efc5f090ce623b
nathanial <nathanial28@gmail.com>
2024-09-10 22:06:01 -0600

    added start button

--- public/body.php
+++ public/body.php
@@ -5,6 +5,4 @@
             <br>
         </div>
          
-        <script src="js/input/keyboard.js" type="module"></script>
-        <script src="js/libs/howler.core.js"></script>
-        <script src="js/libs/pixi.js"></script>
+
--- public/js/graphics/graphics.js
+++ public/js/graphics/graphics.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import {app} from '../graphics/graphics.js';
 
 //const app = new PIXI.Application();
