6601dcd99b6a33fc95f0b7768b4a877cc96fc77e
nathanial <nathanial28@gmail.com>
2024-09-10 22:21:10 -0600

    added start button

--- public/index.php
+++ public/index.php
@@ -7,19 +7,24 @@
     include 'body.php';
 ?>
 
-
-<h1 class="wrapper">Browser RPG</h1>
+<section>
+    <h1 class="wrapper">Browser RPG</h1>
+    <button type="submit" name="startGame" id="startGame btn">Start!</button>
+</section>
 
 <?php 
-    if (isset($_SESSION["useruid"])) {
-        $useruid = htmlspecialchars($_SESSION["useruid"]);
-        echo "<p>Hello there " . "$useruid" . "</p>";
-        //<!-- the game map -->
+    // if (isset($_SESSION["useruid"])) {
+    //     $useruid = htmlspecialchars($_SESSION["useruid"]);
+    //     echo "<p>Hello there " . "$useruid" . "</p>";
+    //     //<!-- the game map -->
+    //     include 'game.php';
+    //     //<!-- the game interface -->
+        
+    //     //<!-- the game chat -->
+        
+    // }
+    if (isset($_POST["startGame"])) {
         include 'game.php';
-        //<!-- the game interface -->
-        
-        //<!-- the game chat -->
-        
     }
     ?>
 
