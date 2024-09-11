ea81deec45406fa117c70f166efab2a6a1175d5e
nathanial <nathanial28@gmail.com>
2024-09-10 21:43:19 -0600

    added start button

--- public/js/frontend/game.js
+++ public/js/frontend/game.js
@@ -1,3 +1,3 @@
-const app = new PIXI.Application();
+export const app = new PIXI.Application();
 await app.init({ width: 640, height: 640 })
 document.getElementById('gameCanvas').appendChild(app.canvas);
