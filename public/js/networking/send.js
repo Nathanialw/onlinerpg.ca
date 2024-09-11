720aa5a219314200fc4ea408c0f6833b1b8f8577
nathanial <nathanial28@gmail.com>
2024-09-11 10:53:04 -0600

    added start button

--- public/js/networking/send.js
+++ public/js/networking/send.js
@@ -5,8 +5,8 @@ const request = new XMLHttpRequest()
 const method = "GET"
 const url = "www.onlinerpg.ca"
 request.open(method, url, true)
-request.send()
-request.log(request)
+//request.send()
+//request.log(request)
 
 export function sendMessage(direction) {
     request.send("0" + direction)
