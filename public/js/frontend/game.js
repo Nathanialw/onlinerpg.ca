2daabbd355ccbcdb6fca1fc0ef2bb4b567799fe8
nathanial <nathanial28@gmail.com>
2024-09-11 12:48:14 -0600

    added ping to keep websocket alive

--- public/js/frontend/game.js
+++ public/js/frontend/game.js
@@ -4,3 +4,8 @@ Init();
 export const app = new PIXI.Application();
 await app.init({ width: 288, height: 288 })
 document.getElementById('gameCanvas').appendChild(app.canvas);
+
+export function closeGame() {
+    app.destroy(true, {children: true, texture: true, baseTexture: true});
+    document.getElementById('gameCanvas').removeChild(app.canvas);    
+}
