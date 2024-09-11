e1596749bb6b51d200de6ef5c2cacf3aa39f1b83
nathanial <nathanial28@gmail.com>
2024-09-11 14:19:37 -0600

    refactored server receiving to update state

--- public/index.php
+++ public/index.php
@@ -7,8 +7,10 @@
     include 'body.php';
 ?>
 
-<h1 class="">Browser RPG</h1>
-<p class="">Welcome to the Browser RPG game. This is a simple text-based game where you can explore the world, fight monsters, and interact with other players. To get started, click the "Start Game" button below.</p>
+<section class="wrapper">
+    <h1 class="">Browser RPG</h1>
+    <p class="">Welcome to the Browser RPG game. This is a simple text-based game where you can explore the world, fight monsters, and interact with other players. To get started, click the "Start Game" button below.</p>
+</section>
 
 <?php 
     // if (isset($_SESSION["useruid"])) {
@@ -28,7 +30,7 @@
         include 'game.php';        
     }
     else {
-        echo "<section><form method='post'>
+        echo "<section class='wrapper'><form method='post'>
                 <input type='submit' name='startGame' id='startGame' class='btn btn-submit' value='Start Game'/>
             </form></section>";
     }
