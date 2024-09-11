834359b964d248a2999789a5187d6d48a52c7a98
nathanial <nathanial28@gmail.com>
2024-09-10 21:45:07 -0600

    added start button

--- public/game.php
+++ public/game.php
@@ -1,2 +1,6 @@
 <div id="gameCanvas"></div>
+
 <script src="js/game.js"></script>
+<script src="js/input/keyboard.js" type="module"></script>
+<script src="js/libs/howler.core.js"></script>
+<script src="js/libs/pixi.js"></script>
