6e2b220b174676a0070215aaac90593cb525e346
nathanial <nathanial28@gmail.com>
2024-09-11 13:06:21 -0600

    added ping to keep websocket alive

--- public/js/frontend/game.js
+++ public/js/frontend/game.js
@@ -6,8 +6,8 @@ await app.init({ width: 288, height: 288 })
 document.getElementById('gameCanvas').appendChild(app.canvas);
 
 export function closeGame() {
-    app.destroy(true, {children: true, texture: true, baseTexture: true});
     document.getElementById('gameCanvas').removeChild(app.canvas);    
+    app.destroy(true, {children: true, texture: true, baseTexture: true});
     document.getElementById('gameCanvas').innerHTML(`<form method='post'>
                 <input type='submit' name='restartGame' id='restartGame' class='wrapper btn btn-submit' value='Restart Game'/>
             </form>`);
