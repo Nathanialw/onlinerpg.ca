f9f351f2dd1459bf1f145e1e285e67667544a856
nathanial <nathanial28@gmail.com>
2024-09-11 11:11:42 -0600

    added start button

--- public/js/networking/send.js
+++ public/js/networking/send.js
@@ -1,9 +1,10 @@
 'use strict'
 
 
-const request = new XMLHttpRequest()
-const method = "GET"
-const url = "www.onlinerpg.ca"
-request.open(method, url, true)
+//const request = new XMLHttpRequest()
+//const method = "GET"
+//const url = "www.onlinerpg.ca"
+//request.open(method, url, true)
+
 //request.send()
 //request.log(request)
