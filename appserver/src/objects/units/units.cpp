0c3a14d1189758c110c8f77537b752a0f34b15ca
nathanial <nathanial28@gmail.com>
2024-09-12 12:09:42 -0600

    camera now moves with the player

--- appserver/src/objects/units/units.cpp
+++ appserver/src/objects/units/units.cpp
@@ -125,10 +125,12 @@ std::string Send_Units() {
 }
 
   void Move(int x, int y) {
+    std::cout << x << " " << y << std::endl;
     for (auto & unit : *Get_Units()) {
       if (unit.type == PLAYER) {
         unit.x += x;
         unit.y += y;
+        std::cout << "x: " << unit.x << " y: " << unit.y << std::endl;
       }
     }
   }
