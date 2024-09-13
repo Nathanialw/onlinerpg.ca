6e589e5886261e22e08639f1a5d6efcd0b2a0264
nathanial <nathanial28@gmail.com>
2024-09-13 13:34:13 -0600

    added wall collision

--- appserver/src/objects/collision/collision.cpp
+++ appserver/src/objects/collision/collision.cpp
@@ -7,42 +7,26 @@
 namespace Collision {
 
   bool Wall_Collision(int x, int y, const char* direction) {
-    std::cout << "basic: " << Map::Get_Map()[0][0] << std::endl;
-    std::cout << "ref: " << &Map::Get_Map()[0][0] << std::endl;
-
-    std::cout << "x: " << x << " y: " << y << std::endl;
-    std::cout << "direction: " << *direction << std::endl;
-    std::cout << "player position Map::Get_Map()[x][y] basic: " << Map::Get_Map()[x][y] << std::endl;
-    std::cout << "player position Map::Get_Map()[x][y] ref: " << &Map::Get_Map()[x][y] << std::endl;
-    std::cout << "player position Map::Get_Map()[x][y] 6 basic: " << Map::Get_Map()[6][6] << std::endl;
-    std::cout << "player position Map::Get_Map()[x][y] 6 ref: " << &Map::Get_Map()[6][6] << std::endl;
+    std::cout << "Map::Get_Map()[0][6]: " << Map::Get_Map()[0][6] << std::endl;
 
     switch (*direction) {
     case 'w':
-      std::cout << "Map::Get_Map()[x][y-1]: " << Map::Get_Map()[x][y-1] << std::endl;
       if (Map::Get_Map()[x][y-1] == '#') {
-        std::cout << "wall collision" << std::endl;
         return true;
         }
         return false;
       case 'a':
-        std::cout << "Map::Get_Map()[x-1][y]: " << Map::Get_Map()[x-1][y] << std::endl;
         if (Map::Get_Map()[x-1][y] == '#') {
-          std::cout << "wall collision" << std::endl;
           return true;
         }
         return false;
       case 's':
-        std::cout << "Map::Get_Map()[x][y+1]: " << Map::Get_Map()[x][y+1] << std::endl;
         if (Map::Get_Map()[x][y+1] == '#') {
-          std::cout << "wall collision" << std::endl;
           return true;
         }
         return false;
       case 'd':
-        std::cout << "Map::Get_Map()[x+1][y]: " << Map::Get_Map()[x+1][y] << std::endl;
         if (Map::Get_Map()[x+1][y] == '#') {
-          std::cout << "wall collision" << std::endl;
           return true;
         }
         return false;
