a1cf84ba07a53dc46001ae614ed6da164cc25895
nathanial <nathanial28@gmail.com>
2024-09-13 13:11:25 -0600

    added wall collision

--- appserver/src/objects/collision/collision.cpp
+++ appserver/src/objects/collision/collision.cpp
@@ -2,29 +2,41 @@
 
 #include "collision.h"
 #include "../../map/map.h"
+#include "iostream"
 
 namespace Collision {
 
   bool Wall_Collision(int x, int y, const char* direction) {
+    std::cout << "x: " << x << " y: " << y << std::endl;
+    std::cout << "direction: " << *direction << std::endl;
+    std::cout << "Map::Get_Map()[x][y]: " << Map::Get_Map()[x][y] << std::endl;
 
     switch (*direction) {
-      case 'w':
-        if (Map::Get_Map()[x][y-1] == '#') {
-          return true;
+    case 'w':
+      std::cout << "Map::Get_Map()[x][y-1]: " << Map::Get_Map()[x][y-1] << std::endl;
+      if (Map::Get_Map()[x][y-1] == '#') {
+        std::cout << "wall collision" << std::endl;
+        return true;
         }
         return false;
       case 'a':
+        std::cout << "Map::Get_Map()[x-1][y]: " << Map::Get_Map()[x-1][y] << std::endl;
         if (Map::Get_Map()[x-1][y] == '#') {
+          std::cout << "wall collision" << std::endl;
           return true;
         }
         return false;
       case 's':
+        std::cout << "Map::Get_Map()[x][y+1]: " << Map::Get_Map()[x][y+1] << std::endl;
         if (Map::Get_Map()[x][y+1] == '#') {
+          std::cout << "wall collision" << std::endl;
           return true;
         }
         return false;
       case 'd':
+        std::cout << "Map::Get_Map()[x+1][y]: " << Map::Get_Map()[x+1][y] << std::endl;
         if (Map::Get_Map()[x+1][y] == '#') {
+          std::cout << "wall collision" << std::endl;
           return true;
         }
         return false;
