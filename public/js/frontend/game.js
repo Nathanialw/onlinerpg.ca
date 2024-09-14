a39e17bc88480ac794f258ee1977ce28d2e7f5c5
nathanial <nathanial28@gmail.com>
2024-09-14 10:55:40 -0600

    add title screen

--- public/js/frontend/game.js
+++ public/js/frontend/game.js
@@ -14,7 +14,7 @@ async function Start_Game() {//13
 
 function Draw_Title_Screen() {
     let title = new PIXI.Text("Online RPG", {fontFamily : "'Press Start 2P'", fontSize: 24, fill : 0xff1010, align : 'center'});
-    title.x = Set_Map_Within_Viewport(11) * cellSize);  // Assuming each cell is 24 pixels tall
+    title.x = Set_Map_Within_Viewport(11) * cellSize;  // Assuming each cell is 24 pixels tall
     title.y = 200;  // Assuming each cell is 24 pixels tall
     app.stage.addChild(title);
     let subtitle = new PIXI.Text("Press space to start!", {fontFamily : "'Press Start 2P'", fontSize: 14, fill : 0xff1010, align : 'center'});
