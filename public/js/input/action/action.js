6f54221099c99d4631c513982f2236767231e7f2
nathanial <nathanial28@gmail.com>
2024-09-14 11:24:38 -0600

    add title screen

--- public/js/input/action/action.js
+++ public/js/input/action/action.js
@@ -8,18 +8,10 @@ import {socket} from "../../networking/socket.js"
 //replace local with server position of they aren't the same
 
 export function Actions(keyName) {
-    console.log("Move");
+    console.log("Enter key pressed");
     let conn = socket()
-
     
-    if (keyName === "Space") {
-        // Move_Player(cell_size, 0);
-        //move camera right
-        if (conn.isConnected) {
-            conn.websocket.send("0Space");       
-        }
-        //send to server
-        return true;
-    }
-    return false;
+    if (conn.isConnected) {
+        conn.websocket.send("0Space");       
+    }    
 }
