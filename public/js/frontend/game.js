a62a5f3576b8f4f80c4d16440d3010439c84f3e7
nathanial <nathanial28@gmail.com>
2024-09-14 02:33:35 -0600

    added name input

--- public/js/frontend/game.js
+++ public/js/frontend/game.js
@@ -5,7 +5,7 @@ export const app = new PIXI.Application();
 await app.init({ width: 24*13, height: 24*13 })
 document.getElementById('gameCanvas').appendChild(app.canvas);
 
-export function closeGame() {
+export function Close_Game() {
     document.getElementById('gameCanvas').removeChild(app.canvas);    
     app.destroy(true, {children: true, texture: true, baseTexture: true});
         // Create a form element
@@ -27,3 +27,59 @@ export function closeGame() {
         document.getElementById('gameCanvas').appendChild(form);
     
 }
+
+
+// import { Init } from "../sound/sound.js";
+// Init();
+
+// export const app = new PIXI.Application();
+// app.init({ width: 24*13, height: 24*13 })
+// document.getElementById('gameCanvas').appendChild(app.canvas);
+
+// function Input_Name() {
+//     let name = prompt("Please enter your name", "Player");    
+// }
+
+// function Character_Select() {
+//     //select race: human or elf
+    
+//     //class fighter
+// }
+
+
+// function Start_Game() {
+// }
+
+// function Splash_Screen() {
+//     //display splash screen
+//     //get name
+//     Input_Name();
+//     //get race
+//     //get class
+//     //start game
+//     Start_Game();
+// }
+// //Splash_Screen()
+
+// export function Close_Game() {
+//     document.getElementById('gameCanvas').removeChild(app.canvas);    
+//     app.destroy(true, {children: true, texture: true, baseTexture: true});
+//         // Create a form element
+//         const form = document.createElement('form');
+//         form.method = 'post';
+    
+//         // Create an input element
+//         const input = document.createElement('input');
+//         input.type = 'submit';
+//         input.name = 'restartGame';
+//         input.id = 'restartGame';
+//         input.className = 'wrapper btn btn-submit';
+//         input.value = 'Restart Game';
+    
+//         // Append the input to the form
+//         form.appendChild(input);
+    
+//         // Append the form to the gameCanvas
+//         document.getElementById('gameCanvas').appendChild(form);
+    
+// }
