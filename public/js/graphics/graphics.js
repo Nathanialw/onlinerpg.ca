306282a2ca67af34f1738798a490b2ac8b62e5ab
nathanial <nathanial28@gmail.com>
2024-09-15 17:12:35 -0600

    add ui windows

--- public/js/graphics/graphics.js
+++ public/js/graphics/graphics.js
@@ -39,18 +39,18 @@ function Get_ViewPort_Origin_y() {
 }
 
 
-function Draw_Game_Window() {
-    let gameWindow = new PIXI.Graphics();
-    gameWindow.beginFill(0x024030);
-    gameWindow.drawRect(0, gameWindowOrigin_y, Set_Window_Width(), Set_Window_Height());
-    gameWindow.endFill();
-    app.stage.addChild(gameWindow);
-}
+// function Draw_Game_Window() {
+//     let gameWindow = new PIXI.Graphics();
+//     gameWindow.beginFill(0x024030);
+//     gameWindow.drawRect(0, gameWindowOrigin_y, Set_Window_Width(), Set_Window_Height());
+//     gameWindow.endFill();
+//     app.stage.addChild(gameWindow);
+// }
 
 function Draw_Top_Panel() {
     let topPanel = new PIXI.Graphics();
     topPanel.beginFill(0x0f0f0f);
-    topPanel.drawRect(0, leftPanelWidth * cellSize, topPanelWidth * cellSize, topPanelHeight * cellSize);
+    topPanel.drawRect(leftPanelWidth * cellSize, gameWindowOrigin_y, topPanelWidth * cellSize, topPanelHeight * cellSize);
     topPanel.endFill();
     app.stage.addChild(topPanel);
 }
@@ -58,7 +58,7 @@ function Draw_Top_Panel() {
 function Draw_Right_Panel() {
     let rightPanel = new PIXI.Graphics();
     rightPanel.beginFill(0x300301);
-    rightPanel.drawRect((viewportWidth + leftPanelWidth) * cellSize, 0, rightPanelWidth * cellSize, rightPanelHeight * cellSize);
+    rightPanel.drawRect((viewportWidth + leftPanelWidth) * cellSize, gameWindowOrigin_y, rightPanelWidth * cellSize, rightPanelHeight * cellSize);
     rightPanel.endFill();
     app.stage.addChild(rightPanel);
 }
@@ -66,7 +66,7 @@ function Draw_Right_Panel() {
 function Draw_Left_Panel() {
     let leftPanel = new PIXI.Graphics();
     leftPanel.beginFill(0x100030);
-    leftPanel.drawRect(0, 0, leftPanelWidth * cellSize, leftPanelHeight * cellSize);
+    leftPanel.drawRect(0, gameWindowOrigin_y, leftPanelWidth * cellSize, leftPanelHeight * cellSize);
     leftPanel.endFill();
     app.stage.addChild(leftPanel);
 }
@@ -74,7 +74,7 @@ function Draw_Left_Panel() {
 function Draw_Bottom_Panel() {
     let bottomPanel = new PIXI.Graphics();
     bottomPanel.beginFill(0x000050);
-    bottomPanel.drawRect(0, (viewportHeight + topPanelHeight) * cellSize, bottomPanelWidth * cellSize, bottomPanelHeight * cellSize);
+    bottomPanel.drawRect(0, gameWindowOrigin_y +(viewportHeight + topPanelHeight) * cellSize, bottomPanelWidth * cellSize, bottomPanelHeight * cellSize);
     bottomPanel.endFill();
     app.stage.addChild(bottomPanel);
 }
@@ -82,32 +82,32 @@ function Draw_Bottom_Panel() {
 function Draw_Map() {
     let map = new PIXI.Graphics();
     map.beginFill(0x009900);
-    map.drawRect(leftPanelWidth * cellSize, topPanelHeight * cellSize, viewportWidth * cellSize, viewportHeight * cellSize);
+    map.drawRect(leftPanelWidth * cellSize, gameWindowOrigin_y + (topPanelHeight * cellSize), viewportWidth * cellSize, viewportHeight * cellSize);
     map.endFill();
     app.stage.addChild(map);
 }
 
-function Draw_Map_Border() {
-    let mapBorder = new PIXI.Graphics();
-    mapBorder.lineStyle(1, 0xffffff, 1);
-    mapBorder.drawRect(leftPanelWidth * cellSize, topPanelHeight * cellSize, viewportWidth * cellSize, viewportHeight * cellSize);
-    app.stage.addChild(mapBorder);
-}
+// function Draw_Map_Border() {
+//     let mapBorder = new PIXI.Graphics();
+//     mapBorder.lineStyle(1, 0xffffff, 1);
+//     mapBorder.drawRect(leftPanelWidth * cellSize, topPanelHeight * cellSize, viewportWidth * cellSize, viewportHeight * cellSize);
+//     app.stage.addChild(mapBorder);
+// }
 
-function Draw_Player_Stats() {
-    let playerStats = new PIXI.Graphics();
-    playerStats.beginFill(0x300400);
-    playerStats.drawRect(0, topPanelHeight * cellSize, leftPanelWidth * cellSize, viewportHeight * cellSize);
-    playerStats.endFill();
-    app.stage.addChild(playerStats);
-}
+// function Draw_Player_Stats() {
+//     let playerStats = new PIXI.Graphics();
+//     playerStats.beginFill(0x300400);
+//     playerStats.drawRect(0, topPanelHeight * cellSize, leftPanelWidth * cellSize, viewportHeight * cellSize);
+//     playerStats.endFill();
+//     app.stage.addChild(playerStats);
+// }
 
-function Draw_Player_Stats_Border() {
-    let playerStatsBorder = new PIXI.Graphics();
-    playerStatsBorder.lineStyle(1, 0xffffff, 1);
-    playerStatsBorder.drawRect(0, topPanelHeight * cellSize, leftPanelWidth * cellSize, viewportHeight * cellSize);
-    app.stage.addChild(playerStatsBorder);
-}
+// function Draw_Player_Stats_Border() {
+//     let playerStatsBorder = new PIXI.Graphics();
+//     playerStatsBorder.lineStyle(1, 0xffffff, 1);
+//     playerStatsBorder.drawRect(0, topPanelHeight * cellSize, leftPanelWidth * cellSize, viewportHeight * cellSize);
+//     app.stage.addChild(playerStatsBorder);
+// }
 
 
 export function Draw_Title_Screen() {
@@ -122,7 +122,7 @@ export function Draw_Title_Screen() {
 }
 
 export async function Create_Canvas() {
-    await app.init({ width: Set_Window_Width(), height: Set_Window_Height() })
+    await app.init({ width: Set_Window_Width(), height: Set_Window_Height() - gameWindowOrigin_y })
     document.getElementById('gameCanvas').appendChild(app.canvas);
 }
 
