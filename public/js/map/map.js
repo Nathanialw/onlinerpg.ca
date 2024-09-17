167c25e099ebbb1020c46f733f477c77ecadc1ba
nathanial <nathanial28@gmail.com>
2024-09-17 13:17:48 -0600

    added bottom map

--- public/js/map/map.js
+++ public/js/map/map.js
@@ -63,7 +63,7 @@ export function Make_Map(serverMap, visionWidth) {
         mapString += mapLine;
         mapDisplay[i] = Create_Map_Line(mapLine, i, visionWidth);
     }
-    Draw_Map();
+    // Draw_Map();
 }
 
 export function Map(data) {
