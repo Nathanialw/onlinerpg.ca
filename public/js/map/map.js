56649978bc7c1aba6a83e7f2ddc3dfb2508f3b27
nathanial <nathanial28@gmail.com>
2024-09-17 12:43:19 -0600

    added bottom map

--- public/js/map/map.js
+++ public/js/map/map.js
@@ -2,6 +2,7 @@
 
 import {Create_Map_Line, Draw_UI, Draw_Vision_Background} from '../graphics/graphics.js';
 import {Set_Enemies, Set_Player, Set_Objects} from '../objects/objects.js';
+import {characterInfo} from '../units/unitdef.js';
 
 let mapDisplay = [];
 
