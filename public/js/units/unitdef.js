09075387ac1e6795ee9bf442bb8260c3b4e397f8
nathanial <nathanial28@gmail.com>
2024-09-17 10:11:52 -0600

    server now send player info to the client

--- public/js/units/unitdef.js
+++ public/js/units/unitdef.js
@@ -57,14 +57,20 @@ let Species = [
     "Ziburinis"
 ]
 
-export let characterInfo;
+export let characterInfo = {
+    Name: "Player",
+    Gender: "default",
+    Species: "default",
+    Class: "default",
+}
+    
 
 export function UpdatePlayerInfo(characterCreate) {
     let length = characterCreate.length;
-    console.log("received characterInfo");
+    console.log("received characterInfo: ", characterCreate);
 
     characterInfo = {
-        name: characterCreate.substring(1, length - 5),
+        Name: characterCreate.substring(4, length - 5),
         Gender: Gender[characterCreate.substring(length - 4, 1)],
         Species: Species[characterCreate.substring(length - 3, 1)],
         Class: Class[characterCreate.substring(length - 2, 1)],
