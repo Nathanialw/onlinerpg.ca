1d53062bbfae64c9487c6396e31c070c96fe364d
nathanial <nathanial28@gmail.com>
2024-09-20 00:01:22 -0600

    now retreives target info from server

--- public/js/frontend/stats.js
+++ public/js/frontend/stats.js
@@ -49,14 +49,14 @@ export function Get_Target_Stats_From_Server(statsString) {
 }
 
 export function Render_Target_Stats() {
-    if (targetStats.target) {
+    if (!targetStats.target) {
         return
     }
     targetStatsDisplay = []
     let x = Get_Right_Panel_Origin_x();
     let y = Get_Right_Panel_Origin_y() + rightPanelWidth;
-    // Load_Target_Image(x, y, targetStats.pic)    
-    //Load_Target_Image(x, y, 'assets/graphics/ui/itsmars_Inventory.png')    
+    Load_Target_Image(x, y, targetStats.pic)    
+    // Load_Target_Image(x, y, 'assets/graphics/ui/itsmars_Inventory.png')    
     targetStatsDisplay[0] = Create_Text_Line("Name: " + targetStats.name, 0, x, y);
     targetStatsDisplay[1] = Create_Text_Line("Gender: " + targetStats.gender, 1, x, y);
     targetStatsDisplay[2] = Create_Text_Line("Alignment: " + targetStats.alignment, 2, x, y);
