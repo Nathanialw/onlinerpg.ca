e251662b511ec20e1ccbb2ae2ef013a14255c84f
nathanial <nathanial28@gmail.com>
2024-09-24 16:06:24 -0600

    added labyrinth map

--- appserver/src/map/map.cpp
+++ appserver/src/map/map.cpp
@@ -46,6 +46,7 @@ namespace Map {
         int charIndex = 0;
 
         if (i == 0 && j == 0) {
+              std::cout << "labyrinthStr: " <<  labyrinthStr << std::endl;
           for (int k = 0; k < 3; k++) {
             for (int l = 0; l < 3; l++) {
               std::cout << (i * 3) + k << " " << (j * 3) + l << std::endl;
