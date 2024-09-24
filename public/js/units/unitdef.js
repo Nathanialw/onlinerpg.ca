47d82e40ec3d9d7680c17f833dfb7b83de3fa87c
nathanial <nathanial28@gmail.com>
2024-09-24 09:19:03 -0600

    added more stats to update request

--- public/js/units/unitdef.js
+++ public/js/units/unitdef.js
@@ -108,14 +108,14 @@ export function UpdatePlayerInfo(characterCreate) {
 
 
     characterInfo.Potrait = characterCreate.substring(length-26, 1), 10;
-    characterInfo.Age = parseInt(characterCreate.substring(length-25, 3), 10);
-    characterInfo.Health = parseInt(characterCreate.substring(length-22, 3), 10);
-    characterInfo.MaxHealth = parseInt(characterCreate.substring(length-19, 3), 10);
-    characterInfo.AC = parseInt(characterCreate.substring(length-16, 2), 10);
-    characterInfo.Speed = parseInt(characterCreate.substring(length-14, 1), 10);
-    characterInfo.MaxSpeed = parseInt(characterCreate.substring(length-13, 1), 10);
-    characterInfo.MinDamage = parseInt(characterCreate.substring(length-10, 3), 10);
-    characterInfo.MaxDamage = parseInt(characterCreate.substring(length-7, 3), 10);
+    characterInfo.Age = characterCreate.substring(length-25, 3), 10;
+    characterInfo.Health = characterCreate.substring(length-22, 3), 10;
+    characterInfo.MaxHealth = characterCreate.substring(length-19, 3), 10;
+    characterInfo.AC = characterCreate.substring(length-16, 2), 10;
+    characterInfo.Speed = characterCreate.substring(length-14, 1), 10;
+    characterInfo.MaxSpeed = characterCreate.substring(length-13, 1), 10;
+    characterInfo.MinDamage = characterCreate.substring(length-10, 3), 10;
+    characterInfo.MaxDamage = characterCreate.substring(length-7, 3), 10;
     characterInfo.Gender = Gender[parseInt(characterCreate.charAt(length-4), 10)];
     characterInfo.Species = Species[parseInt(characterCreate.charAt(length-3), 10)];
     characterInfo.Class = UnitClass[parseInt(characterCreate.charAt(length-2), 10)];
