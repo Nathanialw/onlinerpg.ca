280a8396dbf4a3f93511a130b736780bc3491430
nathanial <nathanial28@gmail.com>
2024-09-25 00:42:33 -0600

    added trees and colors

--- public/header.php
+++ public/header.php
@@ -5,6 +5,8 @@
 <!doctype html>
 <html>
     <head>
+        <link rel="stylesheet" href="/css/game.css">
+        
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -16,6 +18,7 @@
         <link rel="stylesheet" href="/css/footer.css"/>
         <link rel="stylesheet" href="/css/style.css"/>
         <link rel="stylesheet" href="/css/game.css"/>
+        
     </head>
     
     <body>
