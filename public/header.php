4cca5999da9f6c540016e19435c3ccac69998492
nathanial <nathanial28@gmail.com>
2024-09-25 00:48:35 -0600

    added trees and colors

--- public/header.php
+++ public/header.php
@@ -5,8 +5,6 @@
 <!doctype html>
 <html>
     <head>
-        <link rel="stylesheet" href="/css/game.css">
-        
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -18,7 +16,7 @@
         <link rel="stylesheet" href="/css/footer.css"/>
         <link rel="stylesheet" href="/css/style.css"/>
         <link rel="stylesheet" href="/css/game.css"/>
-        
+        <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script>
     </head>
     
     <body>
