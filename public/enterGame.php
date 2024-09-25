16a1d31f27db452b4356918e5056f79aac15898d
nathanial <nathanial28@gmail.com>
2024-09-25 01:00:05 -0600

    added trees and colors

--- public/enterGame.php
+++ public/enterGame.php
@@ -1,5 +1,5 @@
 <section class='startButton'>
-    <form action="/submit" method='post'>
+    <form method='post'>
         <input type='submit' name='enterGame' id='enterGame' class='btn btn-submit btn-center' value='Create Character'/>
     </form>
 </section>
