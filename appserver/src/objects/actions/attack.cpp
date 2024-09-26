67148a792303090fcf7bddc007920a91e621f37a
nathanial <nathanial28@gmail.com>
2024-09-26 00:51:38 -0600

    added melee attack for enemies

--- appserver/src/objects/actions/attack.cpp
+++ appserver/src/objects/actions/attack.cpp
@@ -5,37 +5,27 @@
 #include "iostream"
 #include "map.h"
 #include "units.h"
+#include "spawn.h"
 
 namespace Attack {
 
   //damage done
   //that goblin has died
 Damage Melee(int px, int py, int x, int y) {
-    std::cout << "checking for target" << std::endl;
-    int damageDone = 0;
-    int species;
-    if (Map::Get_Adjacent_Tile(px+x, py+y) == "g") {
-      std::cout << "target found" << std::endl;
-
-      //get the goblin from the units vector
-      auto &unit = Units::Get_Units()->at(Units::Get_Unit_Index(px, py));
-      auto &target = Units::Get_Units()->at(Units::Get_Unit_Index(px+x, py+y));
-      species = (int)target.def.species;
-      //You have attacked a goblin for 5 damage
-
-      target.health -= unit.maxDamage;
-      damageDone = unit.maxDamage;
-      if (target.health <= 0) {
-          std::cout << "target dead" << std::endl;
-
-          //remove the goblin from the unitPositions map
-          Units::Remove_Unit(px+x,py+y);
-          Map::Reset_Tile(px+x, py+y);
-          return {std::to_string(species), damageDone, true}; //move
-      }
-      return {std::to_string(species), damageDone, false}; //don't move
+    //get the goblin from the units vector
+    auto &unit = Units::Get_Units()->at(Units::Get_Unit_Index(px, py));
+    auto &target = Units::Get_Units()->at(Units::Get_Unit_Index(px+x, py+y));
+    //You have attacked a goblin for 5 damage
+    target.health -= unit.maxDamage;
+    std::cout << "You have attacked a: " << Spawn::Get_Unit_Char(target.def.species) << " for " << unit.maxDamage << " damage" << std::endl;
+    if (target.health <= 0) {
+        std::cout << "target dead" << std::endl;
+        //remove the goblin from the unitPositions map
+        Units::Remove_Unit(px+x,py+y);
+        Map::Reset_Tile(px+x, py+y);
+        return {std::to_string((int)target.def.species), unit.maxDamage, true}; //move
     }
-    return {std::to_string(species), damageDone, false}; // move
+    return {std::to_string((int)target.def.species), unit.maxDamage, false}; //don't move
   }
 
 
