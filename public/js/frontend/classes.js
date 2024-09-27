976124186bb0d552dd5c81bce640cc3966f15192
nathanial <nathanial28@gmail.com>
2024-09-26 22:59:26 -0600

    added class names

--- public/js/frontend/classes.js
+++ public/js/frontend/classes.js
@@ -3,12 +3,12 @@ export let classes = new Map([
     ["7000", "Paladin"], ["7001", "Barbarian"], ["7002", "Death Knight"],
     ["7010", "Priest"], ["7011", "Wizard"], ["7012", "Warlock"],
     //female human
-    ["7100", "Valkyrie"], ["7101", "Shield Maiden"], ["7102", "Rogue"],
+    ["7100", "Valkyrie"], ["7101", "Warrior-Maiden"], ["7102", "Rogue"],
     ["7110", "Nun"], ["7111", "Sorceress"], ["7112", "Witch"],
     //male elf
-    ["4000", "Ranger"], ["4001", "Druid"], ["4002", "Vampire"],
-    // ["4010", "Cleric"], ["4011", "Mage"], ["4012", ""],
+    ["4000", "Ranger"], ["4001", "Sentinel"], ["4002", "Vampire"],
+    ["4010", "Druid"], ["4011", "Mage"], ["4012", "Necromancer"],
     //female elf
     // ["4100", ""], ["4101", ""], ["4102", ""],
-    // ["4110", ""], ["4111", ""], ["4112", ""],
+    ["4110", "Fey"], ["4111", "Enchantress"], ["4112", "Wiccan"],
 ]);
