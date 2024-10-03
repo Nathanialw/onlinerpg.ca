3a69f771dd185ee75f51e75087aca4078299714b
nathanial <nathanial28@gmail.com>
2024-10-03 17:40:31 -0600

    level and location data now stored on unit

--- appserver/src/objects/units/update.cpp
+++ appserver/src/objects/units/update.cpp
@@ -73,6 +73,7 @@ namespace Update {
         std::cout << "new position: " << game.objects[level][location].units[0].position.x << ", " << game.objects[level][location].units[0].position.y << std::endl;
         game.objects[game.Get_Player().level][game.Get_Player().location].units[0] = game.objects[level][location].units[0];
         std::cout << "Set position direct: " << game.objects[game.Get_Player().level][game.Get_Player().location].units[0].position.x << ", " << game.objects[game.Get_Player().level][game.Get_Player().location].units[0].position.y << std::endl;
+        std::cout << "Set position game.: " << game.objects[game.level][game.location].units[0].position.x << ", " << game.objects[game.level][game.location].units[0].position.y << std::endl;
         std::cout << "Set position function: " << game.Get_Player().position.x << ", " << game.Get_Player().position.y << std::endl;
 
         Units::Remove_Unit(game.objects[level][location].unitPositions, game.objects[level][location].emptyUnitSlots, formerPos.x, formerPos.y);
