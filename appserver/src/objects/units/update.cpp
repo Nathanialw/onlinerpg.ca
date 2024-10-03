1bfd063fb516db1cb0944382f9d3fc0317356a79
nathanial <nathanial28@gmail.com>
2024-10-03 17:31:56 -0600

    level and location data now stored on unit

--- appserver/src/objects/units/update.cpp
+++ appserver/src/objects/units/update.cpp
@@ -70,11 +70,10 @@ namespace Update {
         std::cout << "Moving to new chunk... " << game.Get_Player().location.x << ", " << game.Get_Player().location.y << std::endl;
         std::cout << "Num units in new zone: " << game.objects[game.Get_Player().level][game.Get_Player().location].units.size() << std::endl;
         std::cout << "old position: " << formerPos.x << ", " << formerPos.y << std::endl;
-        std::cout << "new position: " << game.objects[level][location].units[0].position.x << ", " << game.objects[level][location].units[0].position.y << std::endl;
+        std::cout << "new position: " << game.objects[game.Get_Player().level][game.Get_Player().location].units[0].position.x << ", " << game.objects[level][location].units[0].position.y << std::endl;
+
         game.objects[game.Get_Player().level][game.Get_Player().location].units[0] = game.objects[level][location].units[0];
-        std::cout << "the zero index should always be left open for the player " << std::endl;
         Units::Remove_Unit(game.objects[level][location].unitPositions, game.objects[level][location].emptyUnitSlots, formerPos.x, formerPos.y);
-        std::cout << "Set unit position in hash map as 0 index" << std::endl;
         game.objects[game.Get_Player().level][game.Get_Player().location].unitPositions.emplace(game.Get_Player().position, 0);
         //update map
         Map::Update(game, game.Get_Player().level, formerPos, game.Get_Player().position, location, game.Get_Player().location, Spawn::Get_Unit_Char(game.Get_Player().def.species));
