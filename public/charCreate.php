09402250f5becb653508acdd5b1ed620dce70caf
nathanial <nathanial28@gmail.com>
2024-10-04 12:24:59 -0600

    updated mobile responsive styles

--- public/charCreate.php
+++ public/charCreate.php
@@ -52,27 +52,27 @@
 <section class='options'>    
     <div class="main-links">
         <ul>
-            <li><button id="Human" class='btn btn-block'>Human</button></li>
-            <li><button id="Elf" class='btn btn-block'>Elf</button></li>
+            <li class="option"><button id="Human" class='btn btn-block'>Human</button></li>
+            <li class="option"><button id="Elf" class='btn btn-block'>Elf</button></li>
             <!-- <li><button id="Orc" class='btn btn-submit'>Orc</button></li> -->
             <!-- <li><button id="Dwarf" class='btn btn-submit'>Dwarf</button></li> -->
             <!-- <li><button id="Goblin" class='btn btn-submit'>Goblin</button></li> -->
         </ul>
 
         <ul>
-            <li><button id="Male" class='btn btn-block'>Male</button></li>
-            <li><button id="Female" class='btn btn-block'>Female</button></li>
+            <li class="option"><button id="Male" class='btn btn-block'>Male</button></li>
+            <li class="option"><button id="Female" class='btn btn-block'>Female</button></li>
         </ul>
 
         <ul>
-            <li><button id="Fighter" class='btn btn-block'>Fighter</button></li>
-            <li><button id="Mystic" class='btn btn-block'>Mystic</button></li>
+            <li class="option"><button id="Fighter" class='btn btn-block'>Fighter</button></li>
+            <li class="option"><button id="Mystic" class='btn btn-block'>Mystic</button></li>
         </ul>
 
         <ul>
-            <li><button id="Good" class='btn btn-block'>Good</button></li>
-            <li><button id="Neutral" class='btn btn-block'>Neutral</button></li>
-            <li><button id="Evil" class='btn btn-block'>Evil</button></li>
+            <li class="option"><button id="Good" class='btn btn-block'>Good</button></li>
+            <li class="option"><button id="Neutral" class='btn btn-block'>Neutral</button></li>
+            <li class="option"><button id="Evil" class='btn btn-block'>Evil</button></li>
         </ul>
     </div>
     <h1 id="hero-class"></h1>
