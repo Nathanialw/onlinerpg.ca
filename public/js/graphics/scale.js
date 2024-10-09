f324f2bfbc9686dcea174c53f4b1806a8d0c7e69
nathanial <nathanial28@gmail.com>
2024-10-09 12:40:29 -0600

    now has responsive graphics

--- public/js/graphics/scale.js
+++ public/js/graphics/scale.js
@@ -11,29 +11,34 @@ const mediaQueries = [
     { query: window.matchMedia('(max-width: 3840px)'), width: 3840 }
 ];
 
+function Update8() {
+    Set_Font_Size(8);
+    Update_Screen()
+}
+function Update12() {
+    Set_Font_Size(12);
+    Update_Screen()
+}
+function Update16() {
+    Set_Font_Size(16);
+    Update_Screen()
+}
+function Update24() {
+    Set_Font_Size(24);
+    Update_Screen()
+}
+
+let Update = {
+    1280: Update8,
+    1920: Update12,
+    2560: Update16,
+    3840: Update24,
+}
+
 function handleMediaQueryChange() {
     // Loop through the media queries and log the matched width
-    mediaQueries.forEach(({ query, width }) => {
-        if (query.matches) {
-            console.log(`Media Query Matched! Width: ${width}px`);
-        }
-        if (query.matches && width === 1280) {
-            Set_Font_Size(8);
-            Update_Screen()
-        }
-        else if (query.matches && width === 1920) {
-            Set_Font_Size(12);
-            Update_Screen()
-        }
-        else if (query.matches && width === 2560) {
-            Set_Font_Size(16);
-            Update_Screen()
-        }
-        else if (query.matches && width === 3840) {
-            Set_Font_Size(24);
-            Update_Screen()
-        }        
-    });
+    Update[width]();        
+    console.log('Matched width:', width);
 }
 
 // Register event listeners and perform initial check
