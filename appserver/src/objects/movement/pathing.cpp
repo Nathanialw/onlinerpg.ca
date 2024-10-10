d590460fd639aac50f668458f41d8af4db372429
nathanial <nathanial28@gmail.com>
2024-10-09 19:50:34 -0600

    pathing maps are now connected

--- appserver/src/objects/movement/pathing.cpp
+++ appserver/src/objects/movement/pathing.cpp
@@ -196,7 +196,7 @@ namespace Pathing {
       //connect left
       for (int i = 0; i < Component::mapWidth * Component::mapWidth; i += Component::mapWidth) {
         std::cout << "i: " << i << std::endl;
-        std::cout << "i + Component::mapWidth: " << i + Component::mapWidth << std::endl;
+        std::cout << "i + Component::mapWidth - 1: " << i + Component::mapWidth - 1 << std::endl;
         std::cout << "total mapSize: " << Component::mapWidth * Component::mapWidth << std::endl;
           chunk[i].vecNeighbours.push_back(&toConnect[i + Component::mapWidth - 1]);
           toConnect[i + Component::mapWidth - 1].vecNeighbours.push_back(&chunk[i]);
@@ -217,19 +217,19 @@ namespace Pathing {
           std::cout << "i: " << i << std::endl;
           std::cout << "((Component::mapWidth * Component::mapWidth) - Component::mapWidth) + i: " << ((Component::mapWidth * Component::mapWidth) - Component::mapWidth) + i << std::endl;
           std::cout << "total mapSize: " << Component::mapWidth * Component::mapWidth << std::endl;
-          chunk[i].vecNeighbours.push_back(&toConnect[((Component::mapWidth * Component::mapWidth) - Component::mapWidth) + i]);
-          toConnect[((Component::mapWidth * Component::mapWidth) - Component::mapWidth) + i].vecNeighbours.push_back(&chunk[i]);
+          chunk[((Component::mapWidth * Component::mapWidth) - Component::mapWidth) + i].vecNeighbours.push_back(&toConnect[i]);
+          toConnect[i].vecNeighbours.push_back(&chunk[((Component::mapWidth * Component::mapWidth) - Component::mapWidth) + i]);
       }
       std::cout << "connected up" << std::endl;
     }
     else if (y == 1) {
       //connect down
-      for (int i = (Component::mapWidth * Component::mapWidth) - Component::mapWidth; i < Component::mapWidth * Component::mapWidth; i++) {
+      for (int i = 0; i < Component::mapWidth; i++) {
           std::cout << "i: " << i << std::endl;
-          std::cout << "i - ((Component::mapWidth * Component::mapWidth) - Component::mapWidth): " << i - ((Component::mapWidth * Component::mapWidth) - Component::mapWidth) << std::endl;
+          std::cout << "i - ((Component::mapWidth * Component::mapWidth) - Component::mapWidth): " << (Component::mapWidth * Component::mapWidth) - Component::mapWidth + i << std::endl;
           std::cout << "total mapSize: " << Component::mapWidth * Component::mapWidth << std::endl;
-          chunk[i - ((Component::mapWidth * Component::mapWidth) - Component::mapWidth)].vecNeighbours.push_back(&toConnect[i]);
-          toConnect[i].vecNeighbours.push_back(&chunk[i - ((Component::mapWidth * Component::mapWidth) - Component::mapWidth)]);
+          chunk[i].vecNeighbours.push_back(&toConnect[(Component::mapWidth * Component::mapWidth) - Component::mapWidth + i]);
+          toConnect[(Component::mapWidth * Component::mapWidth) - Component::mapWidth + i].vecNeighbours.push_back(&chunk[i]);
       }
     }
     std::cout << "connected down" << std::endl;
