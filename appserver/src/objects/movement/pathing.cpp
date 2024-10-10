6b00270661ab5f18bec055da8a8f31b80cb181d1
nathanial <nathanial28@gmail.com>
2024-10-09 19:16:59 -0600

    pathing maps are now connected

--- appserver/src/objects/movement/pathing.cpp
+++ appserver/src/objects/movement/pathing.cpp
@@ -204,10 +204,10 @@ namespace Pathing {
       //connect right
       for (int i = Component::mapWidth - 1; i < Component::mapWidth * Component::mapWidth; i += Component::mapWidth) {
           std::cout << "i: " << i << std::endl;
-          std::cout << "i - Component::mapWidth: " << i - Component::mapWidth << std::endl;
+          std::cout << "i - Component::mapWidth: " << i - (Component::mapWidth - 1) << std::endl;
           std::cout << "total mapSize: " << Component::mapWidth * Component::mapWidth << std::endl;
-          chunk[i].vecNeighbours.push_back(&toConnect[i - Component::mapWidth]);
-          toConnect[i - Component::mapWidth].vecNeighbours.push_back(&chunk[i]);
+          chunk[i].vecNeighbours.push_back(&toConnect[i - (Component::mapWidth - 1)]);
+          toConnect[i - (Component::mapWidth - 1)].vecNeighbours.push_back(&chunk[i]);
       }
       std::cout << "connected right" << std::endl;
     }
