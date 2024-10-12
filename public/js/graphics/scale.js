9f8c61e77d6cbc3d77d76a01d47634a2139f495d
nathanial <nathanial28@gmail.com>
2024-10-12 01:40:35 -0600

    enemies now drop items

--- public/js/graphics/scale.js
+++ public/js/graphics/scale.js
@@ -9,8 +9,10 @@ const mediaQueries = [
     { query: window.matchMedia('(min-width: 712px) and (max-width: 1279px)'), size: 8 },
     { query: window.matchMedia('(min-width: 1280px) and (max-width: 1919px)'), size: 12 },
     { query: window.matchMedia('(min-width: 1920px) and (max-width: 2559px)'), size: 16 },
-    { query: window.matchMedia('(min-width: 2560px) and (max-width: 3839px)'), size: 24 },
-    { query: window.matchMedia('(min-width: 3840px)'), size: 24 }
+    // { query: window.matchMedia('(min-width: 2560px) and (max-width: 3839px)'), size: 24 },
+    { query: window.matchMedia('(min-width: 2560px) and (max-width: 3239px)'), size: 24 },
+    { query: window.matchMedia('(min-width: 3240px) and (max-width: 3839px)'), size: 28 },
+    { query: window.matchMedia('(min-width: 3840px)'), size: 32 }
 ];
 
 function handleMediaQueryChange() {
---
+++ public/js/objects/loot.js
@@ -0,0 +1,7 @@
+'use strict'
+
+//when I move on to a new tile
+//check if there is loot
+//if there is get the indexes of the items
+//read from db
+//display the icon in the loot box
