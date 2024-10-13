3dc6ae5adcd7c7576996358dd72b6200c52ade59
nathanial <nathanial28@gmail.com>
2024-10-13 14:09:30 -0600

    sqlite3 and websocketpp are now statically linked

--- appserver/src/db/db.cpp
+++ appserver/src/db/db.cpp
@@ -26,10 +26,19 @@ namespace DB {
       sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr);
       std::cout << "step" << std::endl;
 
-      sqlite3_step(stmt);
+      int rc = sqlite3_step(stmt);
       std::cout << "step success" << std::endl;
 
-      std::string result = std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+      std::string result;
+      if (rc == SQLITE_ROW) {
+        const unsigned char *text = sqlite3_column_text(stmt, 0);
+        if (text) {
+          result = std::string(reinterpret_cast<const char *>(text));
+        } else {
+          result = ""; // or handle the null case as needed
+        }
+      }
+
       std::cout << "result: " << result << std::endl;
 
       sqlite3_finalize(stmt);
