89e2531e7ae67012a711cb442532861a6aa6b56b
nathanial <nathanial28@gmail.com>
2024-10-13 02:31:56 -0600

    handles full inventory

--- public/js/objects/inventory.js
+++ public/js/objects/inventory.js
@@ -14,7 +14,7 @@ export async function Draw_Invetory(items) {
 
 
     for (let i = 0; i < items.length; i++) {
-        let item = await Draw_Inventory_Icons(items[i], i, 1, 2.5, 2.5)        
+        let item = await Draw_Inventory_Icons(items[i], i, 1, 1, 2.5)        
         Set_Send_On_Loot_Click_Listener(item, '1', i);   //1 means inventory panel
     }
 }
