99b3399714fc56456428d3b454f29ebbf9fd407a
nathanial <nathanial28@gmail.com>
2024-10-12 21:07:10 -0600

    added click listener to loot

--- public/js/networking/send.js
+++ public/js/networking/send.js
@@ -20,14 +20,14 @@ export function Set_Send_On_Map_Click_Listener(objectDisplay, x, y) {
     }); //query the server for the object data
 }
 
-export function Set_Send_On_Loot_Click_Listener(objectDisplay, i) {
+export function Set_Send_On_Loot_Click_Listener(item, i) {
     //send the index of the item in the loot array
     item.eventMode = 'static';
     item.cursor = 'pointer';
 
     let message = "2" + i;
 
-    objectDisplay.on('pointerdown', (event) => { 
+    item.on('pointerdown', (event) => { 
         Send_Web_Socket_Message(message); 
         console.log("Click Message sent: ", message); 
     }); //query the server for the object data
