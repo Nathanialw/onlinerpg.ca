4e0b7696e6deb22fe451928b437e31bf0de5d060
nathanial <nathanial28@gmail.com>
2024-10-14 02:05:16 -0600

    added a few more items to the db

--- public/js/graphics/graphics.js
+++ public/js/graphics/graphics.js
@@ -1,6 +1,6 @@
 'use strict'
 import { characterInfo } from '../units/unitdef.js';
-import * as PIXI from 'js/libs/pixi.js';
+import * as PIXI from '../libs/pixi.js';
 
 export const app = new Application();
 
