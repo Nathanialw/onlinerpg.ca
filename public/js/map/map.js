6ce0608ac03e4d7c8be6b388cdd50edae0dcc4d5
nathanial <nathanial28@gmail.com>
2024-10-15 16:50:50 -0600

    items now have a sprite

--- public/js/map/map.js
+++ public/js/map/map.js
@@ -190,7 +190,9 @@ export function Make_Map(serverMap, visionWidth) {
             serverMap = serverMap.substring(0, i) + " " + serverMap.substring(i + 1); //background char
         }
         else if ((serverMap[i] == ",")) {
-            serverMap = serverMap.substring(0, i) + "ô" + serverMap.substring(i + 1); //background char
+            // serverMap = serverMap.substring(0, i) + "ô" + serverMap.substring(i + 1); //background char
+            objects.push([i, "ô"]); // save the location of the enemy
+            serverMap = serverMap.substring(0, i) + " " + serverMap.substring(i + 1); //background char
         }
         else {
             objects.push([i, serverMap[i]]); // save the location of the enemy
