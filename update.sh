a10e249ba219eb67c7623f97fd1ddde61792b852
nathanial <nathanial28@gmail.com>
2024-10-15 17:41:24 -0600

    added hover over items

--- update.sh
+++ update.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <restart_process> "
+    exit 1
+fi
+
+# Assign arguments to variables
+RESTART=$1
+
 git checkout -- *
 git pull
 
@@ -8,7 +19,9 @@ git pull
 
 chmod +x /var/www/onlinerpg.ca/appserver/release/BrowserRPG
 chmod 666 /var/www/onlinerpg.ca/appserver/db/data.db
-sudo service rpgserver restart
-sudo service rpgserver status
-bg
+if [ "$RESTART" == "restart" ]; then
+    sudo service rpgserver restart
+    sudo service rpgserver status
+    bg
+fi
 # sudo service rpgserver restart
