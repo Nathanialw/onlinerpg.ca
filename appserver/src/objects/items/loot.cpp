fd9511fd3aac376841bb004426198638e2a9dd4b
nathanial <nathanial28@gmail.com>
2024-10-14 22:04:36 -0600

    updated db

--- appserver/src/objects/items/loot.cpp
+++ appserver/src/objects/items/loot.cpp
@@ -12,10 +12,12 @@ namespace Loot {
   void Generate_Loot(std::array<uint8_t , 4> &items) {
     uint8_t numItems = Utils::Random(1, 4);
     for (int i = 0; i < numItems; ++i) {
-      uint8_t itemID = Utils::Random(1 , 21);
+      uint8_t itemID = Utils::Random(1 , 23);
+      std::cout << "item dropped: " << itemID << std::endl;
+      if (itemID == 23)
+        std::cout << "IT DID IN FACT WORK WIT WILL DROP AT 23 AS THE MAX VALUE " << itemID << std::endl;
       items[i] = itemID;
     }
-    std::cout << "items dropped" << std::endl;
   }
 
   std::string Query_Loot(std::array<uint8_t , 4> &items) {
