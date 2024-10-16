821659b8dd55964167b4e582e02c76d4563f6e9c
nathanial <nathanial28@gmail.com>
2024-10-15 19:40:14 -0600

    added right click

--- public/js/frontend/tooltip.js
+++ public/js/frontend/tooltip.js
@@ -12,11 +12,18 @@ export async function Draw_Tooltip(x, y) {
     let numLines = 12;
     tooltip = new PIXI.Sprite(tooltipTexture);
     Draw_Sprite(x, y, maxLengthLine * cellSize, numLines * cellSize, tooltip);
-    Create_Text_Line("This is a test", cellSize, 1, x, y);
-    Create_Text_Line("This is a test line 2", cellSize, 2, x, y);
+    lines[0] = Create_Text_Line("This is a test", cellSize, 1, x, y);
+    lines[1] = Create_Text_Line("This is a test line 2", cellSize, 2, x, y);
 }
 
 export function Remove_Tooltip() {
+    if (lines) {
+        for (let i = 0; i < lines.length; i++) {
+            app.stage.removeChild(lines[i]);
+        }
+        lines.length = 0;
+    }
+
     if (tooltip) {
         app.stage.removeChild(tooltip);
         tooltip = null;
