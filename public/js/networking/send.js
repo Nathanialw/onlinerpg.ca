276fe30f715af1292fc3b683e15d06ca3838e1f9
nathanial <nathanial28@gmail.com>
2024-10-15 18:12:26 -0600

    added hover over items

--- public/js/networking/send.js
+++ public/js/networking/send.js
@@ -85,7 +85,7 @@ export function Set_Send_On_Loot_Click_Listener(item, panel, i) {
         // Optionally, you can handle right mouse button click event
     });
 
-    item.addEventListener('contextmenu', (event) => {
+    item.on('rightclick', (event) => {
         event.preventDefault();
         event.stopPropagation();
         console.log("Context menu prevented");
