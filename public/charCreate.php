76cd99d7f097f5323c84e978ef3825467f69893f
nathanial <nathanial28@gmail.com>
2024-10-23 13:22:40 -0600

    added connect button to start screen

--- public/charCreate.php
+++ public/charCreate.php
@@ -51,52 +51,6 @@
     <div id="gameCanvas"></div>
 </section>
 
-<section class="options-container">
-    <article class='options'>    
-        <ul>
-            <li class="option"><button id="Human" class='btn btn-block'>Human</button></li>
-            <li class="option"><button id="Elf" class='btn btn-block'>Elf</button></li>
-            <!-- <li><button id="Orc" class='btn btn-submit'>Orc</button></li> -->
-            <!-- <li><button id="Dwarf" class='btn btn-submit'>Dwarf</button></li> -->
-            <!-- <li><button id="Goblin" class='btn btn-submit'>Goblin</button></li> -->
-        </ul>
-
-        <ul>
-            <li class="option"><button id="Male" class='btn btn-block'>Male</button></li>
-            <li class="option"><button id="Female" class='btn btn-block'>Female</button></li>
-        </ul>
-
-        <ul>
-            <li class="option"><button id="Fighter" class='btn btn-block'>Fighter</button></li>
-            <li class="option"><button id="Mystic" class='btn btn-block'>Mystic</button></li>
-        </ul>
-
-        <ul>
-            <li class="option"><button id="Good" class='btn btn-block'>Good</button></li>
-            <li class="option"><button id="Neutral" class='btn btn-block'>Neutral</button></li>
-            <li class="option"><button id="Evil" class='btn btn-block'>Evil</button></li>
-        </ul>
-    </section>
-</section>
-
-<section class="hero-wrapper">
-    <h1 id="hero-class" class="hero-class"></h1>
-</section>
-
-<section class='nameInput'>
-    <form method='POST' id="myForm">
-        <input class="form-input form-row" type="text" id="name" name="name" value="" required minlength="3" maxlength="20" autocomplete="off">
-        <br> 
-    </form>
-</section>
-
-<section class='startButton button'>
-    <button id="startGame" class='btn btn-center'>Start Game</button>
-    <br> 
-    <div id="nameError" class="error-message" style="color: red; display: none;">Name must be at least 3 characters long.</div>
-    <div id="loading-text" class="loading-message" style="color: white; display: none;">Loading...</div>
-</section>
-
 <section class='connectButton button'>
     <button id="connect" class='btn btn-center'>Connect</button>
     <br> 
