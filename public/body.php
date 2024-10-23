414a2e31fa98b41b4ecbf76eb300f08eca026485
nathanial <nathanial28@gmail.com>
2024-10-23 16:55:36 -0600

    added connect button to start screen

--- public/body.php
+++ public/body.php
@@ -1,5 +1,7 @@
 
 <section class="wrapper">
     <h1 class="splashTitle">Browser RPG</h1>
-    <p class="splashContent">Welcome to the Browser RPG game. This is an ASCII-graphics game where you can explore the world and fight monsters. To get started, click the "Start Game" button below.</p>
+    <p class="splashContent">Welcome to the Browser RPG game. 
+                             This is an ASCII-graphics game where you can explore the world and fight monsters. 
+                             To get started, click Connect below.</p>
 </section>
