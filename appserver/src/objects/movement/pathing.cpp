26a57a8e978222b07ca0a25edf36e4c27749dc74
nathanial <nathanial28@gmail.com>
2024-10-25 23:29:43 -0600

    Game is now a class

--- appserver/src/objects/movement/pathing.cpp
+++ appserver/src/objects/movement/pathing.cpp
@@ -203,6 +203,10 @@ namespace Pathing {
     if (x == 1) {
       //connect right
       for (int i = Component::mapWidth - 1; i < Component::mapWidth * Component::mapWidth; i += Component::mapWidth) {
+          std::cout << "i: " << i << std::endl;
+          std::cout << "i - (Component::mapWidth - 1): " << i - (Component::mapWidth - 1) << std::endl;
+          std::cout << "total mapSize: " << Component::mapWidth * Component::mapWidth << std::endl;
+
           chunk[i].vecNeighbours.push_back(&toConnect[i - (Component::mapWidth - 1)]);
           toConnect[i - (Component::mapWidth - 1)].vecNeighbours.push_back(&chunk[i]);
       }
