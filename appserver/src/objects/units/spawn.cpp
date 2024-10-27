f212b77033562515c42a5a2525287784ba100edc
nathanial <nathanial28@gmail.com>
2024-10-27 04:47:13 -0600

    Item is anow a class

--- appserver/src/objects/units/spawn.cpp
+++ appserver/src/objects/units/spawn.cpp
@@ -12,152 +12,153 @@
 
 namespace Spawn {
 
-  char unitChars[(int)Units::Species::SIZE] = {'a',   'b',    'c',
-                                               'd',   'e',    'f',    'g',
-                                               'h',   'i',    'j',    'k',
-                                               'l',   'm',    'n',    'o',
-                                               'p',   'q',    'r',    's',
-                                               't',   'u',    'v',    'w',
-                                               'x',   'y',    'z',    'A',
-                                               'B',   'C',    'D',    'E',   'F',
-                                               'G',   'H',    'I',    'J',   'K',
-                                               'L',   'M',    'N',    'O',   'P',
-                                               'Q',   'R',    'S',    'T',   'U',
-                                               'V',   'W',    'X',    'Y',   'Z'};
+     char unitChars[(int) Units::Species::SIZE] = {'a', 'b', 'c',
+                                                   'd', 'e', 'f', 'g',
+                                                   'h', 'i', 'j', 'k',
+                                                   'l', 'm', 'n', 'o',
+                                                   'p', 'q', 'r', 's',
+                                                   't', 'u', 'v', 'w',
+                                                   'x', 'y', 'z', 'A',
+                                                   'B', 'C', 'D', 'E', 'F',
+                                                   'G', 'H', 'I', 'J', 'K',
+                                                   'L', 'M', 'N', 'O', 'P',
+                                                   'Q', 'R', 'S', 'T', 'U',
+                                                   'V', 'W', 'X', 'Y', 'Z'};
 
-  char Get_Unit_Char(Units::Species species) {
-    return unitChars[(int)species];
-  }
+     char Get_Unit_Char(Units::Species species) {
+	     return unitChars[(int) species];
+     }
 
-  void Add_Unit(Units::Objects &objects, int level, Component::Position location, Component::Position position, const uint16_t &name, Units::Gender gender, Units::Species species, Units::Class unitClass, Units::Alignment alignment, uint8_t picNum) {
-    Units::Unit unit(level, location);
+     void Add_Unit(Units::Objects &objects, int level, Component::Position location, Component::Position position, const uint16_t &name, Units::Gender gender, Units::Species species, Units::Class unitClass, Units::Alignment alignment, uint8_t picNum) {
+	     Units::Unit unit(level, location);
 
-    //defined by where it is spawned
-    unit.def.species = species;
-    unit.stats.unitID = (uint8_t)species + 1;
-    unit.position.position = position;
+	     //defined by where it is spawned
+	     unit.def.species = species;
+	     unit.stats.unitID = (uint8_t) species + 1;
+	     unit.position.position = position;
 
-    //randomize, weighted
-    unit.def.gender = gender; //unless it is a gendered species
-    unit.def.unitClass = unitClass;
+	     //randomize, weighted
+	     unit.def.gender = gender; //unless it is a gendered species
+	     unit.def.unitClass = unitClass;
 
-    //query db for all of this
-    unit.def.alignment = alignment;
-    unit.name.firstName = name;
-    unit.stats.age = 16;
+	     //query db for all of this
+	     unit.def.alignment = alignment;
+	     unit.name.firstName = name;
+	     unit.stats.age = 16;
 
-    unit.stats.speed = 1;
-    unit.stats.maxSpeed = unit.stats.speed;
-    unit.stats.minDamage = 0;
-    unit.stats.maxDamage = 10;
-    unit.stats.vision = 6;
-    unit.stats.AC = 10;
-    unit.stats.health = 30;
-    unit.stats.healthMax = unit.stats.health;
+	     unit.stats.speed = 1;
+	     unit.stats.maxSpeed = unit.stats.speed;
+	     unit.stats.minDamage = 0;
+	     unit.stats.maxDamage = 10;
+	     unit.stats.vision = 6;
+	     unit.stats.AC = 10;
+	     unit.stats.health = 30;
+	     unit.stats.healthMax = unit.stats.health;
 
-    auto &units = objects.units;
-    auto &unitsPositions = objects.unitPositions;
+	     auto &units = objects.units;
+	     auto &unitsPositions = objects.unitPositions;
 
-    auto &emptyUnitSlots = objects.emptyUnitSlots;
-    if (emptyUnitSlots.empty()) {
-      units.emplace_back(unit);
-    } else {
-      units.at(emptyUnitSlots.back()) = unit;
-      emptyUnitSlots.pop_back();
-    }
-    unitsPositions.emplace(position, units.size() - 1);
-  }
+	     auto &emptyUnitSlots = objects.emptyUnitSlots;
+	     if (emptyUnitSlots.empty()) {
+		     units.emplace_back(unit);
+	     } else {
+		     units.at(emptyUnitSlots.back()) = unit;
+		     emptyUnitSlots.pop_back();
+	     }
+	     unitsPositions.emplace(position, units.size() - 1);
+     }
 
-  std::vector<Component::Position> Spawn_Unit(std::vector<Chunk::Room> &rooms) {
-    std::vector<Component::Position> placements;
-    Component::Position placement{};
-    Proc_Gen::Seed seed;
+     std::vector<Component::Position> Spawn_Unit(std::vector<Chunk::Room> &rooms) {
+	     std::vector<Component::Position> placements;
+	     Component::Position placement{};
+	     Proc_Gen::Seed seed;
 
-    for (const auto &room : rooms) {
-      seed.seed = Proc_Gen::Create_Initial_Seed(room.x, room.y);
-      placement.x = Proc_Gen::Random_Int(room.x, room.x + room.w, seed);
-      placement.y = Proc_Gen::Random_Int(room.y, room.y + room.h, seed);
-      placements.emplace_back(placement);
-    }
-    return placements;
-  }
+	     for (const auto &room: rooms) {
+		     seed.seed = Proc_Gen::Create_Initial_Seed(room.x, room.y);
+		     placement.x = Proc_Gen::Random_Int(room.x, room.x + room.w, seed);
+		     placement.y = Proc_Gen::Random_Int(room.y, room.y + room.h, seed);
+		     placements.emplace_back(placement);
+	     }
+	     return placements;
+     }
 
-  bool Add_Object(Units::Objects &objects, int level, Component::Position location, std::string &group, Component::Position position) {
-    if (position.x > Component::mapWidth - 1 || position.y > Component::mapWidth - 1 || position.x < 1 || position.y < 1) {
-      group.clear();
-      return false;
-    } else {
-      group += Utils::Prepend_Zero(position.x);
-      group += Utils::Prepend_Zero(position.y);
+     bool Add_Object(Units::Objects &objects, int level, Component::Position location, std::string &group, Component::Position position) {
+	     if (position.x > Component::mapWidth - 1 || position.y > Component::mapWidth - 1 || position.x < 1 || position.y < 1) {
+		     group.clear();
+		     return false;
+	     } else {
+		     group += Utils::Prepend_Zero(position.x);
+		     group += Utils::Prepend_Zero(position.y);
 
-      auto species = (Units::Species)Utils::Random(0, (int)Units::Species::SIZE - 1);
-      //check db if the species is gendered
-      auto gendered = DB::Query("isGendered", "units", "name", Units::species[(int)species]);
-      Units::Gender gender;
-      (gendered == "1") ? gender = (Units::Gender)Utils::Random(0, (int)Units::Gender::SIZE - 1) : gender = Units::Gender::FEMALE;
-      auto unitClass = (Units::Class)Utils::Random(0, (int)Units::Class::SIZE - 1);
-      auto alignment = (Units::Alignment)Utils::Random(0, (int)Units::Alignment::SIZE - 1);
+		     auto species = (Units::Species) Utils::Random(0, (int) Units::Species::SIZE - 1);
+		     //check db if the species is gendered
+		     auto gendered = DB::Query("isGendered", "units", "name", Units::species[(int) species]);
+		     Units::Gender gender;
+		     (gendered == "1") ? gender = (Units::Gender) Utils::Random(0, (int) Units::Gender::SIZE - 1) : gender = Units::Gender::FEMALE;
+		     auto unitClass = (Units::Class) Utils::Random(0, (int) Units::Class::SIZE - 1);
+		     auto alignment = (Units::Alignment) Utils::Random(0, (int) Units::Alignment::SIZE - 1);
 
-      //count the number of images in the assets folder and return a random number
-      uint8_t picNum = Files::Get_Random_Pic("../public/assets/graphics/imgs/" + Units::species[(int)species] + "/" + Units::gender[(int)gender][0] + "/");
+		     //count the number of images in the assets folder and return a random number
+		     uint8_t picNum = Files::Get_Random_Pic("../public/assets/graphics/imgs/" + Units::species[(int) species] + "/" + Units::gender[(int) gender][0] + "/");
 
-//      std::vector<std::pair<std::string, std::string>> whereEquals = {{"race", Units::species[(int)species]}, {"type", Units::gender[(int)gender]}};
-      std::vector<std::pair<std::string, std::string>> whereEquals = {{"race", Units::species[(int)Units::Species::ORC]}, {"type", Units::gender[(int)gender]}};
-      auto names = DB::Get_List("uID", "names", whereEquals);
-      uint16_t index = Utils::Random(0, (int)names.size() - 1);
+		     //      std::vector<std::pair<std::string, std::string>> whereEquals = {{"race", Units::species[(int)species]}, {"type", Units::gender[(int)gender]}};
+		     std::vector<std::pair<std::string, std::string>> whereEquals = {{"race", Units::species[(int) Units::Species::ORC]},
+		                                                                     {"type", Units::gender[(int) gender]}};
+		     auto names = DB::Get_List("uID", "names", whereEquals);
+		     uint16_t index = Utils::Random(0, (int) names.size() - 1);
 
-      Add_Unit(objects, level, location, position, stoi(names[index]), gender, species, unitClass, alignment, picNum);
-      return true;
-    }
-  }
+		     Add_Unit(objects, level, location, position, stoi(names[index]), gender, species, unitClass, alignment, picNum);
+		     return true;
+	     }
+     }
 
-  std::string Random_Entities(Units::Objects &objects, int level, Component::Position location, const char entityType, int numEntities, int x, int y) {
-    std::string group;
-    int tries = 10;
-  reRoll:
-    if (tries == 0) {
-      return group;
-    }
-    for (int i = 0; i < numEntities; ++i) {
-      group += entityType;
-      Component::Position position(x + i , y + i);
-      if (!Add_Object(objects, level, location, group, position)) {
-        tries--;
-        goto reRoll;
-      }
-    }
-    return group;
-  }
+     std::string Random_Entities(Units::Objects &objects, int level, Component::Position location, const char entityType, int numEntities, int8_t x, int8_t y) {
+	     std::string group;
+	     auto tries = 10;
+     reRoll:
+	     if (tries == 0) {
+		     return group;
+	     }
+	     for (auto i = 0; i < numEntities; ++i) {
+		     group += entityType;
+		     Component::Position position(x + i, y + i);
+		     if (!Add_Object(objects, level, location, group, position)) {
+			     tries--;
+			     goto reRoll;
+		     }
+	     }
+	     return group;
+     }
 
-  void Place_Entities_On_Map(std::vector<Chunk::Room> &rooms, int level, Component::Position location, Units::Objects &objects) {
-    auto placements = Spawn_Unit(rooms);
-    std::cout << "num placements: " << placements.size() << std::endl;
+     void Place_Entities_On_Map(std::vector<Chunk::Room> &rooms, int level, Component::Position location, Units::Objects &objects) {
+	     auto placements = Spawn_Unit(rooms);
+	     std::cout << "num placements: " << placements.size() << std::endl;
 
-    for (const auto &placement : placements) {
-      int numMonsters = Utils::Random(0,3);
-      auto unitData = Random_Entities(objects, level, location, unitChars[(int)Units::Species::GOBLIN], numMonsters, placement.x, placement.y);
+	     for (const auto &placement: placements) {
+		     int numMonsters = Utils::Random(0, 3);
+		     auto unitData = Random_Entities(objects, level, location, unitChars[(int) Units::Species::GOBLIN], numMonsters, placement.x, placement.y);
 //      objects.unitsString += unitData;
-      std::cout << "units added: " << unitData << std::endl;
-    }
+		     std::cout << "units added: " << unitData << std::endl;
+	     }
 
-    std::cout << "init num entities: " << objects.units.size() << std::endl;
-  }
+	     std::cout << "init num entities: " << objects.units.size() << std::endl;
+     }
 
-  //we beed the chunk that the unit is being spawned in
-  void Init(int level, Component::Position location, Chunk::Chunk chunk, std::vector<Chunk::Room> &rooms, Units::Objects &objects) {
+     //we beed the chunk that the unit is being spawned in
+     void Init(int level, Component::Position location, Chunk::Chunk chunk, std::vector<Chunk::Room> &rooms, Units::Objects &objects) {
 
-    if (objects.units.empty()) { //To save the 0 index for the player
-      Units::Unit playerPlaceholder(level, location);
-      objects.units.emplace_back(playerPlaceholder);
-    }
-    Place_Entities_On_Map(rooms, level, location, objects);
+	     if (objects.units.empty()) { //To save the 0 index for the player
+		     Units::Unit playerPlaceholder(level, location);
+		     objects.units.emplace_back(playerPlaceholder);
+	     }
+	     Place_Entities_On_Map(rooms, level, location, objects);
 
-    for (auto &unit : objects.units) {
-      if (unit.stats.health > 0) {
-        Map::Set_Tile(chunk, unit.position.position.x, unit.position.position.y, unitChars[(int)unit.def.species]);
-      }
-    }
-    std::cout << "units inited" << std::endl;
+	     for (auto &unit: objects.units) {
+		     if (unit.stats.health > 0) {
+			     Map::Set_Tile(chunk, unit.position.position.x, unit.position.position.y, unitChars[(int) unit.def.species]);
+		     }
+	     }
+	     std::cout << "units inited" << std::endl;
 
-  }
+     }
 }
--- appserver/src/objects/units/species/goblin.cpp
+++ appserver/src/objects/units/species/goblin.cpp
@@ -21,49 +21,49 @@
 //type? (fire/ice/etc maybe not sure if will use)
 
 namespace Species {
-  std::string Get_Unit_Data_As_string(Game::Instance &game, const std::string &msg) {
-    int8_t mapPositionX = game.Get_Player().position.position.x - game.Get_Player().stats.vision + std::stoi(msg.substr(0, 2));
-    int8_t mapPositionY = game.Get_Player().position.position.y - game.Get_Player().stats.vision + std::stoi(msg.substr(2, 2));
-    Component::Position position = {mapPositionX, mapPositionY};
+     std::string Get_Unit_Data_As_string(Game::Instance &game, const std::string &msg) {
+	     int8_t mapPositionX = game.Get_Player().position.position.x - game.Get_Player().stats.vision + std::stoi(msg.substr(0, 2));
+	     int8_t mapPositionY = game.Get_Player().position.position.y - game.Get_Player().stats.vision + std::stoi(msg.substr(2, 2));
+	     Component::Position position = {mapPositionX, mapPositionY};
 
-    //will need to use mouse coords to determine level/location, but for now you can only get from the same map chunk/level as the player
-    auto unit = Units::Get_Unit_At_Position(game.Get_Objects().units, game.Get_Objects().unitPositions, position);
+	     //will need to use mouse coords to determine level/location, but for now you can only get from the same map chunk/level as the player
+	     auto unit = Units::Get_Unit_At_Position(game.Get_Objects().units, game.Get_Objects().unitPositions, position);
 
-    //unit ID for the DB
-    std::cout << "unitID  int8: " << unit.stats.unitID << std::endl;
-    auto unitID = Utils::Prepend_Zero_By_Digits(unit.stats.unitID, 3);
-    std::cout << "unitID str: " << unitID << std::endl;
-        //name uID from "names" table, save in unit class
-      //vision from DB
-      //speed from DB
-      //alignment from DB
-      //bio from DB
-      //pic from DB
+	     //unit ID for the DB
+	     std::cout << "unitID  int8: " << unit.stats.unitID << std::endl;
+	     auto unitID = Utils::Prepend_Zero_By_Digits(unit.stats.unitID, 3);
+	     std::cout << "unitID str: " << unitID << std::endl;
+	     //name uID from "names" table, save in unit class
+	     //vision from DB
+	     //speed from DB
+	     //alignment from DB
+	     //bio from DB
+	     //pic from DB
 
-    //append to the message
-      //age random generate
-      //health random generate + level + items
-      //gender random generate
-      //damage random generate + items
-      //AC random generate + items
+	     //append to the message
+	     //age random generate
+	     //health random generate + level + items
+	     //gender random generate
+	     //damage random generate + items
+	     //AC random generate + items
 
-    std::cout << "unit name int16: " << unit.name.firstName << std::endl;
-    auto name = Utils::Prepend_Zero_By_Digits(unit.name.firstName, 4);
-    std::cout << "unit name str: " << name << std::endl;
+	     std::cout << "unit name int16: " << unit.name.firstName << std::endl;
+	     auto name = Utils::Prepend_Zero_By_Digits(unit.name.firstName, 4);
+	     std::cout << "unit name str: " << name << std::endl;
 
-    auto age = Utils::Prepend_Zero_By_Digits(unit.stats.age, 3);
-    std::string gender = std::to_string((int)unit.def.gender);
+	     auto age = Utils::Prepend_Zero_By_Digits(unit.stats.age, 3);
+	     std::string gender = std::to_string((int) unit.def.gender);
 //    std::string alignment = std::to_string((int)unit.def.alignment);
-    //assets/species/goblin.png   this would be a good opportunity to try using a CDN
+	     //assets/species/goblin.png   this would be a good opportunity to try using a CDN
 //    auto pic = "5";
-    auto health = Utils::Prepend_Zero_3Digit(unit.stats.health) + Utils::Prepend_Zero_3Digit(unit.stats.healthMax);
-    auto damage = Utils::Prepend_Zero(unit.stats.minDamage) + Utils::Prepend_Zero(unit.stats.maxDamage);
-    auto AC = Utils::Prepend_Zero(unit.stats.AC);
-    auto pic = Utils::Prepend_Zero_By_Digits(unit.stats.picNum, 2);
+	     auto health = Utils::Prepend_Zero_3Digit(unit.stats.health) + Utils::Prepend_Zero_3Digit(unit.stats.healthMax);
+	     auto damage = Utils::Prepend_Zero(unit.stats.minDamage) + Utils::Prepend_Zero(unit.stats.maxDamage);
+	     auto AC = Utils::Prepend_Zero(unit.stats.AC);
+	     auto pic = Utils::Prepend_Zero_By_Digits(unit.stats.picNum, 2);
 //    auto speed = std::to_string(unit.speed);
 //    auto vision = Utils::Prepend_Zero(unit.vision);
 //    std::string bio = "Swamp goblins inhabit the murky, mist-shrouded wetlands of the Shadowlands, where they thrive in the damp, oxygen-poor environment. These peculiar creatures have adapted to their surroundings, developing scaly, moss-covered skin that blends seamlessly with the surrounding vegetation. Their eyes glow like lanterns in the dark, emitting a faint, eerie light that allows them to navigate the treacherous, waterlogged terrain with ease. Their limbs are long and spindly, ending in webbed fingers and toes that enable them to propel themselves through the murky waters with surprising agility.";
 //    return name + "_" + age + gender + alignment + pic + "_" + health + damage + AC + speed + vision + bio;
-    return unitID + name + age + gender + health + damage + AC + pic;
-  }
+	     return unitID + name + age + gender + health + damage + AC + pic;
+     }
 }
