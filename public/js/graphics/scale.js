b7414b46c87fc0a83b5066cbb678fabb8d4f3d13
nathanial <nathanial28@gmail.com>
2024-10-27 18:38:00 -0600

    removed side bars from game screen

--- public/js/graphics/scale.js
+++ public/js/graphics/scale.js
@@ -4,11 +4,11 @@ import { Set_Font_Size, Set_Cell_Size } from './graphics.js';
 import { Update_Screen, Update_Screen_Phone } from '../frontend/ui.js';
 
 const mediaQueries = [
-    { query: window.matchMedia('(min-width: 1px) and (max-width: 390px)'), size: 10 }, //originally 8, added +2 to all of them to try to fit screen
-    { query: window.matchMedia('(min-width: 391px) and (max-width: 711px)'), Size: 10 },
-    { query: window.matchMedia('(min-width: 712px) and (max-width: 1279px)'), Size: 10 },
-    { query: window.matchMedia('(min-width: 1280px) and (max-width: 1919px)'), Size: 10 },
-    { query: window.matchMedia('(min-width: 1920px) and (max-width: 2559px)'), Size: 10 },
+    { query: window.matchMedia('(min-width: 1px) and (max-width: 390px)'), size: Set_Cell_Size(390) }, //originally 8, added +2 to all of them to try to fit screen
+    { query: window.matchMedia('(min-width: 391px) and (max-width: 711px)'), Size: Set_Cell_Size(390) },
+    { query: window.matchMedia('(min-width: 712px) and (max-width: 1279px)'), Size: Set_Cell_Size(712) },
+    { query: window.matchMedia('(min-width: 1280px) and (max-width: 1919px)'), Size: Set_Cell_Size(1280) },
+    { query: window.matchMedia('(min-width: 1920px) and (max-width: 2559px)'), Size: Set_Cell_Size(1920) },
     // { query: window.matchMedia('(min-width: 2560px) and (max-width: 3839px)'), size: 24 },
     { query: window.matchMedia('(min-width: 2560px) and (max-width: 3239px)'), size: Set_Cell_Size(2560) },
     { query: window.matchMedia('(min-width: 3240px) and (max-width: 3839px)'), size: Set_Cell_Size(3240) },
