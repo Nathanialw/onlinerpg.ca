8dde5fcfc8def27b63da458d3b8d131d6c96afdd
nathanial <nathanial28@gmail.com>
2024-10-27 18:33:53 -0600

    removed side bars from game screen

--- public/js/graphics/scale.js
+++ public/js/graphics/scale.js
@@ -20,6 +20,7 @@ const mediaQueries = [
 function handleMediaQueryChange() {
     // Loop through the media queries in descending order and log the matched size
     for (let { query, size } of mediaQueries) {
+        console.log("cellsize: ", size)
         if (query.matches) {
             Set_Font_Size(size);
             Update_Screen()
