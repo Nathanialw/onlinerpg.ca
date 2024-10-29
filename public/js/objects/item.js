daa736008d93d9f096189c93735d838a7965c55f
nathanial <nathanial28@gmail.com>
2024-10-29 14:16:54 -0600

    updated stats parsing

--- public/js/objects/item.js
+++ public/js/objects/item.js
@@ -12,25 +12,6 @@ export let item = {
     IconPath: "none"
 };
 
-export function Parse_Item(dataStr, item) {
-    [item.ItemID, dataStr] = [parseInt(dataStr.substring(0, 3)), dataStr.substring(3)];
-    //save rarity border path
-    [item.Rarity, dataStr] = [parseInt(dataStr.substring(0, 1)), dataStr.substring(1)];
-    //save durability value
-    [item.Durability, dataStr] = [parseInt(dataStr.substring(0, 3)), dataStr.substring(3)];
-    
-    //sav modifier uIDs to look up in the db
-    const numMods = parseInt(dataStr.substring(0, 1));
-    dataStr = dataStr.substring(1);
-
-    for (let j = 0; j < numMods; j++) {
-        item.Modifiers.push(parseInt(dataStr.substring(0, 3)));
-        dataStr = dataStr.substring(3)
-    }
-
-    return dataStr;
-}
-
 
 let modType = [
     "damage",
