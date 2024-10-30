f33269f7a98a9d3b87f79ed323e9639f9f2f42da
nathanial <nathanial28@gmail.com>
2024-10-29 23:12:53 -0600

    update loot parsing

--- public/js/parse/loot.js
+++ public/js/parse/loot.js
@@ -7,7 +7,7 @@ export function Parse_Loot(dataStr, loot) {
     loot.NumItems = parseInt(dataStr.substring(0, 2));
     dataStr = dataStr.substring(2)
 
-    for (let i = 0; i < numItems; i++) {
+    for (let i = 0; i < loot.NumItems; i++) {
         dataStr = Parse_Item(dataStr, loot.Items[i]);
     }
     return dataStr;
