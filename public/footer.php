3086e768cf207a5b14aa02ac44a477404bd06f54
nathanial <nathanial28@gmail.com>
2024-11-16 11:36:27 -0700

    updated footer icons

--- public/footer.php
+++ public/footer.php
@@ -2,10 +2,6 @@
 		<footer>
 			<div class='footer'>
 				<ul class='social-icons'>
-					<li><a target='_blank' href='https://facebook.com'><i class='fab fa-facebook'></i></a></li>
-					<li><a target='_blank' href='https://twitter.com'><i class='fab fa-twitter'></i></a></li>
-					<li><a target='_blank' href='https://instagram.com'><i class='fab fa-instagram'></i></a></li>
-					<li><a target='_blank' href='https://youtube.com'><i class='fab fa-youtube'></i></a></li>
 					<li><a target='_blank' href='https://github.com/nathanialw'><i class='fab fa-github'></i></a></li>
 				</ul>
 			</div>
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to handle termination signals
 cleanup() {
     echo "Stopping servers..."
     # Add any cleanup commands here
@@ -8,18 +7,17 @@ cleanup() {
     exit 0
 }
 
-# Trap termination signals
 trap cleanup SIGINT SIGTERM
 
-# Start the main server in the background
+# start the server in the background
 # cd /mnt/BED0BFC1D0BF7DE9/Workspace/onlinerpg.ca/appserver/ || { echo "Failed to change directory"; exit 1; }
 # ./release/BrowserRPG &
 
-# Start the PHP built-in server in the background
+# this is for the PHP to run
 php -S localhost:9010 -t public &
 
-# Open the default web browser to localhost:9010
+# open the default web browser to localhost:9010
 xdg-open http://localhost:9010
 
-# Wait for both background processes to finish
+# should make both close when one is closed
 wait
