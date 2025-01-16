cfd18b284803e08db80c7e903e4c3249e116326c
nathanial <nathanial28@gmail.com>
2025-01-16 10:57:49 -0700

    updated build

---
+++ README.md
@@ -0,0 +1,11 @@
+Thusfar only tested on a lunix machine running Ubuntu
+
+Currently hosted at https://www.onlinerpg.ca
+
+Required libraries:
+-sudo apt install libwebsocketpp-dev
+-sudo apt install build-essential g++ python3-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev
+
+ -Navigate to onlinerpg.ca directory
+ -./run.sh #command will:    build   ->  Open a browser at the correct port  ->  Run the backend application
+ -You may pass a port humber as a argument, otherwise it will default to port 9010
--- run.sh
+++ run.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
+cd appserver
+mkdir release
+cd release
+cmake ..
+make
+cd ..
+cd ..
 
 cleanup() {
     echo "Stopping servers..."
-    # Add any cleanup commands here
     kill $(jobs -p)
     exit 0
 }
 
 trap cleanup SIGINT SIGTERM
 
-# start the server in the background
-# cd /mnt/BED0BFC1D0BF7DE9/Workspace/onlinerpg.ca/appserver/ || { echo "Failed to change directory"; exit 1; }
-# ./release/BrowserRPG &
+#checking it the arguemnt is a valid port number
+if [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -ge 1 ] && [ "$1" -le 65535 ]; then
+    port=$1
+else
+    echo "Invalid port number. Please provide a port number between 1 and 65535. Defaulting to 9010."
+    port=9010
+fi
 
 # this is for the PHP to run
-php -S localhost:9010 -t public &
+php -S localhost:$port -t public &
 
-# open the default web browser to localhost:9010
-xdg-open http://localhost:9010
+# start the server in the background
+cd appserver || { echo "Failed to change directory"; exit 1; }
+./release/BrowserRPG &
+
+# open the default web browser to localhost:$port
+xdg-open http://localhost:$port
 
 # should make both close when one is closed
 wait
